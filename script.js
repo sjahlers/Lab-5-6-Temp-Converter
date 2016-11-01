ab357c85beb5d784d2382280e78b34ebf8e4b510
Sara Ahlers <sjahlers@gmail.com>
2016-11-01 10:25:56 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function convertTemp (temp, scale) {
+	if (scale === "C") {
+      return ((temp - 32) * (5/9));
+    }
+    else {
+      return ((temp * (9/5) + 32));
+    }
+ }
+
+var result = convertTemp(212, "C");
+console.log(result);
+
+/*C = ((temp - 32) * (5/9))
+F = (temp * (9/5) + 32))*/
